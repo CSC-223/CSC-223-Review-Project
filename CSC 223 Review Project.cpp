a11f2a542ff55a1a1b405e7ba05bbc3fceae5236
Sally Schaffner <schaffners@vpcc.edu>
2024-08-28 11:14:32 -0400

    Add project files.

---
+++ CSC 223 Review Project.cpp
@@ -0,0 +1,10 @@
+// CSC 223 Review Project.cpp : This contains the code for the Review 
+// which will be on three branches, PointType Class, CircleType Class, CylinderType class
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "CSC 223 Review Project\n";
+}
